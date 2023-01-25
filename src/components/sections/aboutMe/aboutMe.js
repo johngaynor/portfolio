bd42445f8421faa65e8e144530b617a515d8ccc7
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-01-25 00:15:24 -0500

    styling changes

--- src/components/sections/aboutMe/aboutMe.js
+++ src/components/sections/aboutMe/aboutMe.js
@@ -1,65 +1,47 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 import pfp from "../../../assets/images/gaynor-pfp.JPEG";
 import styles from "./aboutMe.css";
 
-function AboutMe(currentTab) {
-  const [show, setShow] = useState(false);
-  useEffect(() => {
-    if (currentTab.currentTab === 0) {
-      setShow(true);
-    } else {
-      setShow(false);
-    }
-  }, [currentTab]);
+function AboutMe() {
   return (
-    <>
-      {show ? (
-        <div className="about-container">
-          <div className="about-box w-75">
-            <div className="about-profile">
-              <img src={pfp} alt="" />
-            </div>
-            <div className="about-text">
-              <div className="about-skills">
-                <span>JavaScript</span>
-                <span>jQuery</span>
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>SASS</span>
-                <span>Bootstrap</span>
-                <span>React</span>
-                <span>Firebase</span>
-                <span>MVC</span>
-                <span>Node.js</span>
-              </div>
-              <h3 className="about-header">
-                An Eye for{" "}
-                <span className="header-orange-red">Optimization</span>
-              </h3>
-              <p className="subtitle pe-4">
-                In my years as a competitive Rubik's Cube solver, I discovered
-                an interest in using technology to optimize human tasks. This
-                branched into a passion for developing applications that make
-                life easier for its users.
-              </p>
-              <p className="subtitle pe-4">
-                This led to me pursuing a degree in Informatics at IUPUI
-                (current sophomore). I have since completed IUPUI's Web & Mobile
-                Development Boot Camp and continue to learn and develop my own
-                projects. I'm still in my early stages as a developer but I
-                believe I have the passion and determination to change the world
-                someday.
-              </p>
-              {/* <p className="subtitle pe-4">
-                Outside of school and coding I spend most of my free time in the
-                gym or developing new skills.
-              </p> */}
-            </div>
-          </div>
+    <div className="about-container">
+      <div className="about-box w-75">
+        <div className="about-profile">
+          <img src={pfp} alt="" />
         </div>
-      ) : null}
-    </>
+        <div className="about-text">
+          <div className="about-skills">
+            <span>JavaScript</span>
+            <span>jQuery</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>SASS</span>
+            <span>Bootstrap</span>
+            <span>React</span>
+            <span>Firebase</span>
+            <span>MVC</span>
+            <span>Node.js</span>
+          </div>
+          <h1 className="about-header">
+            An Eye for <span className="header-orange-red">Optimization</span>
+          </h1>
+          <h5>
+            In my years as a competitive Rubik's Cube solver, I discovered an
+            interest in using technology to optimize human tasks. This branched
+            into a passion for developing applications that make life easier for
+            its users. I'm still in my early stages as a developer but I believe
+            I have the passion and determination to change the world someday.
+          </h5>
+        </div>
+      </div>
+
+      <h4>
+        My Projects <FontAwesomeIcon icon={faArrowDown} className="mx-2" />
+      </h4>
+    </div>
   );
 }
 
--- src/components/sections/hero/hero.js
+++ src/components/sections/hero/hero.js
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 import MyProjects from "../myProjects/myProjects";
 import AboutMe from "../aboutMe/aboutMe";
 import styles from "./hero.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faCode,
+  faCodeBranch,
+  faCodeCommit,
+  faDumbbell,
+  faFileCode,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Hero() {
   const [activeTab, setActiveTab] = useState(0);
@@ -15,34 +24,40 @@ function Hero() {
     window.scrollTo(1000, document.body.scrollHeight);
   };
 
-  //   adding an underline to the active tab
-  useEffect(() => {
-    const headers = Array.from(document.querySelectorAll(".hero-links h5"));
-    headers.forEach((header, index) => {
-      header.classList.remove("active");
-      if (index === activeTab) {
-        header.classList.add("active");
-      }
-    });
-  }, [activeTab]);
-
   return (
     <>
       <div className="hero-container">
-        <div className="w-75">
-          <h3 className="">Hey, I'm John Gaynor.</h3>
-          <h1 className="ms-lg-4 subtitle">
+        <FontAwesomeIcon icon={faCode} className="hero-icons closing-tag" />
+        <FontAwesomeIcon
+          icon={faCodeBranch}
+          className="hero-icons code-branch"
+        />
+        <FontAwesomeIcon
+          icon={faCodeCommit}
+          className="hero-icons code-commit"
+        />
+        <FontAwesomeIcon icon={faFileCode} className="hero-icons code-file" />
+        <FontAwesomeIcon icon={faDumbbell} className="hero-icons icon-weight" />
+        <div className="hero-links">
+          <h5 onClick={setAbout}>ABOUT</h5>
+          <h5 onClick={setProjects}>PROJECTS</h5>
+          <h5>CONNECT</h5>
+        </div>
+        <div className="w-75 subtitles">
+          <h3>Hey, I'm John Gaynor.</h3>
+          <h1 className="subtitle middle">
             My <span>passion</span> is building solutions for websites and
             applications.
           </h1>
-          <div className="hero-links d-flex justify-content-between">
-            <h5 onClick={setAbout}>ABOUT</h5>
-            <h5 onClick={setProjects}>PROJECTS</h5>
-            <h5>CONNECT</h5>
-          </div>
+          <h1 className="subtitle smaller mb-5 pb-5">
+            Oh, and lifting heavy stuff off the ground for fun.
+          </h1>
+          <h4>
+            About Me <FontAwesomeIcon icon={faArrowDown} className="mx-2" />
+          </h4>
         </div>
       </div>
-      <AboutMe currentTab={activeTab}></AboutMe>
+      <AboutMe></AboutMe>
       <MyProjects currentTab={activeTab}></MyProjects>
     </>
   );
